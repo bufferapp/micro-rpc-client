5e0a84fe90ad7589f120b83f6f504807c3a589c9
Harrison Harnisch <hharnisc@gmail.com>
2018-09-28 15:23:18 -0500

    add custom error code

--- __mocks__/isomorphic-fetch.js
+++ __mocks__/isomorphic-fetch.js
@@ -1,24 +1,39 @@
 const fakeMethods = ['methods', 'test'];
 const fakeResponse = 'fake response';
+const fakeCode = 1001;
 const fetch = jest.fn((url, options) => {
   if (JSON.parse(options.body).name === 'methods') {
     return Promise.resolve({
       status: 200,
-      json: () => Promise.resolve({
-        result: fakeMethods,
-      }),
+      json: () =>
+        Promise.resolve({
+          result: fakeMethods,
+        }),
     });
   } else if (JSON.parse(options.body).name === 'shouldThrow') {
     return Promise.resolve({
       status: 400,
-      statusText: 'this method should throw an error',
+      json: () =>
+        Promise.resolve({
+          error: 'this method should throw an error',
+        }),
+    });
+  } else if (JSON.parse(options.body).name === 'shouldThrowCustomCode') {
+    return Promise.resolve({
+      status: 400,
+      json: () =>
+        Promise.resolve({
+          error: 'this method should throw an error',
+          code: fakeCode,
+        }),
     });
   }
   return Promise.resolve({
     status: 200,
-    json: () => Promise.resolve({
-      result: fakeResponse,
-    }),
+    json: () =>
+      Promise.resolve({
+        result: fakeResponse,
+      }),
   });
 });
 fetch.fakeMethods = fakeMethods;
--- index.test.js
+++ index.test.js
@@ -3,42 +3,36 @@ const RPCClient = require('./index');
 
 describe('RPCClient', () => {
   it('should create an RPCClient', () => {
-    expect(new RPCClient())
-      .toBeDefined();
+    expect(new RPCClient()).toBeDefined();
   });
 
   it('should set RPC server url', () => {
     const url = 'http://some-rpc-server';
     const rpc = new RPCClient({ url });
-    expect(rpc.url)
-      .toBe(url);
+    expect(rpc.url).toBe(url);
   });
 
   it('should set default RPC server url', () => {
     const rpc = new RPCClient();
-    expect(rpc.url)
-      .toBe('http://localhost');
+    expect(rpc.url).toBe('http://localhost');
   });
 
   describe('listMethods', () => {
     it('should list available RPC methods', () => {
       const rpc = new RPCClient();
-      return rpc.listMethods()
-        .then((methods) => {
-          expect(fetch)
-            .toBeCalledWith('http://localhost', {
-              method: 'POST',
-              headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                name: 'methods',
-              }),
-            });
-          expect(methods)
-            .toEqual(fetch.fakeMethods);
+      return rpc.listMethods().then((methods) => {
+        expect(fetch).toBeCalledWith('http://localhost', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: 'methods',
+          }),
         });
+        expect(methods).toEqual(fetch.fakeMethods);
+      });
     });
   });
 
@@ -46,58 +40,63 @@ describe('RPCClient', () => {
     it('should call RPC method', () => {
       const name = 'someMethod';
       const rpc = new RPCClient();
-      return rpc.call(name)
-        .then((response) => {
-          expect(fetch)
-            .toBeCalledWith('http://localhost', {
-              method: 'POST',
-              headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                name,
-              }),
-            });
-          expect(response)
-            .toEqual(fetch.fakeResponse);
+      return rpc.call(name).then((response) => {
+        expect(fetch).toBeCalledWith('http://localhost', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+          }),
         });
+        expect(response).toEqual(fetch.fakeResponse);
+      });
     });
 
     it('should call RPC method with args', () => {
       const name = 'someMethod';
       const args = { a: 'a', b: 'b' };
       const rpc = new RPCClient();
-      return rpc.call(name, args)
-        .then((response) => {
-          expect(fetch)
-            .toBeCalledWith('http://localhost', {
-              method: 'POST',
-              headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                name,
-                args: JSON.stringify(args),
-              }),
-            });
-          expect(response)
-            .toEqual(fetch.fakeResponse);
+      return rpc.call(name, args).then((response) => {
+        expect(fetch).toBeCalledWith('http://localhost', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+            args: JSON.stringify(args),
+          }),
         });
+        expect(response).toEqual(fetch.fakeResponse);
+      });
     });
 
-    it('should trigger error when response !== 200', () => {
+    it('should trigger error when response !== 200', async () => {
+      expect.assertions(2);
       const name = 'shouldThrow';
       const rpc = new RPCClient();
-      return rpc.call(name)
-        .then(() => {
-          throw new Error('this should not happen');
-        })
-        .catch((err) => {
-          expect(err)
-            .toEqual(new Error('this method should throw an error'));
-        });
+      try {
+        await rpc.call(name);
+      } catch (err) {
+        expect(err.handled).toBe(true);
+        expect(err.message).toBe('this method should throw an error');
+      }
+    });
+
+    it('should trigger error when response !== 200 with custom code', async () => {
+      expect.assertions(2);
+      const name = 'shouldThrowCustomCode';
+      const rpc = new RPCClient();
+      try {
+        await rpc.call(name);
+      } catch (err) {
+        expect(err.message).toBe('this method should throw an error');
+        expect(err.code).toBe(fetch.fakeCode);
+      }
     });
   });
 });
